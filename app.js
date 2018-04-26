33cec638904209d396ea8c8819e54f808dd763c6
Đăng Khoa <khoaace@gmail.com>
2018-04-26 17:25:14 +0700

    update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.listen(3000);
+app.listen(process.env.PORT||3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
